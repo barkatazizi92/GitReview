c22d240155093e440482811005afc331c8eacf09
barkatazizi92 <barkatmilad@gmail.com>
2024-04-11 14:48:25 -0400

    1 commit

---
+++ src/test/java/gitPractice.java
@@ -0,0 +1,7 @@
+public class gitPractice {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("How is you life going");
+    }
+
+}
